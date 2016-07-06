c8abf261211b05a281fe4d7e9d5cf5bbd7310a7a
Shailesh Humbad <shailesh@atensoftware.com>
2016-07-06 16:37:58 -0400

    First commit

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Shailesh Humbad
+ * @copyright Copyright (c) 2016 Aten Software LLC (https://www.atensoftware.com)
+ * @package AtenSoftware_ProductExport
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+	ComponentRegistrar::MODULE,
+	'AtenSoftware_ProductExport',
+	__DIR__
+);
+
+?>
